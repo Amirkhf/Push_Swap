23664f0075e9062e77392cf54e8f635e39e98dc9
Amir Khelifi <amkhelif@f1br10s1.clusters.42paris.fr>
2025-12-04 14:09:26 +0100

    parsing 0/10

---
+++ parsing/ft_utils_parsing.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_utils_parsing.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amkhelif <amkhelif@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/04 12:36:11 by amkhelif          #+#    #+#             */
+/*   Updated: 2025/12/04 14:08:14 by amkhelif         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../Push_Swap.h"
+
+long long	ft_atoi(char *str)
+{
+	int	i;
+	int	sign;
+	long long	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while ((str[i] == ' ') || (str[i] >= '0' && str[i] <= '9'))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
+
+
